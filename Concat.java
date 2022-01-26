b6ba9d3d47266be5c6b32f6cc8c4f3d3831cc45a
Vijay Kedar <54878107+kedarvijay@users.noreply.github.com>
2022-01-26 22:38:41 +0530

    Create Concat.java

---
+++ Concat.java
@@ -0,0 +1,6 @@
+package com.concat;
+
+public interface Concat {
+	String concat(String first_name,String middle_name, String last_name);
+// we have declare one abstract method with three string arguments.
+}
